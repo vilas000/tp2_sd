957314df4395c4699bc2c52a9867c410b92e7572
Gustavo Ferreira <gustavo.hcf@aluno.ufop.edu.br>
2025-07-31 15:56:04 -0300

    adicionados comentarios explicativos

--- client_app/client.py
+++ client_app/client.py
@@ -4,19 +4,19 @@ import random
 import datetime
 import json
 import os
-import sys # Para saída de erro robusta
+import sys 
 
-# --- Configurações do Cliente ---
-# Obtém o ID do cliente e o host/porta do Cluster Sync das variáveis de ambiente.
-# Isso garante que o Docker Compose pode configurar cada cliente dinamicamente.
+# configuracoes iniciais do cliente
+# CLIENT_ID eh o nome unico do cliente, pode ser definido via variavel de ambiente ou gerado aleatoriamente
+# CLUSTER_SYNC_HOST e CLUSTER_SYNC_PORT definem o endereco do no do cluster sync com o qual esse cliente deve se comunicar
 CLIENT_ID = os.getenv("CLIENT_ID", f"Cliente_{random.randint(1000, 9999)}")
 CLUSTER_SYNC_HOST = os.getenv("CLUSTER_SYNC_HOST", '127.0.0.1')
 
 try:
     CLUSTER_SYNC_PORT = int(os.getenv("CLUSTER_SYNC_PORT", "0"))
     if CLUSTER_SYNC_PORT == 0:
-        # Fallback para desenvolvimento local se a porta não for definida no ambiente
-        # Mas para Docker Compose, ela deve ser sempre definida.
+        # fallback para desenvolvimento local se a porta não for definida no ambiente
+        # para Docker Compose, ela deve ser sempre definida.
         print(f"[{CLIENT_ID}] Aviso: CLUSTER_SYNC_PORT não definida no ambiente, usando porta padrão 12345.", file=sys.stderr)
         CLUSTER_SYNC_PORT = 12345
 except (ValueError, TypeError):
@@ -25,18 +25,26 @@ except (ValueError, TypeError):
 
 
 def iniciar_cliente():
+    '''
+    funcao com toda a logica de comportamento do cliente.
+    '''
     print(f"[{CLIENT_ID}]: Iniciando e conectando a {CLUSTER_SYNC_HOST}:{CLUSTER_SYNC_PORT}...")
     
+    # antes de comecar a enviar requisições, aguarda um tempo aleatório para que nem todos os 5 clientes enviem os primeiros pedidos ao mesmo tempo
     initial_delay = random.uniform(5, 10)
     print(f"[{CLIENT_ID}]: Aguardando {initial_delay:.2f} segundos antes de enviar requisições...")
     time.sleep(initial_delay)
 
-    num_acessos = 1 
-    #num_acessos = random.randint(10, 50)  # Conforme especificação
+    # cada cliente determina quantas vezes tentara acessar o recurso compartilhado aleatoriamente
+    #num_acessos = 1 
+    num_acessos = random.randint(10, 50)  
     print(f"[{CLIENT_ID}]: Fará {num_acessos} acessos ao Recurso R.")
 
+    # aqui comeca o ciclo de vida do cliente, que se repetira ate que o numero de acessos desejado seja atingido
     acessos_realizados = 0
     for i in range(num_acessos):
+
+        # gera um timestamp preciso para o momento, que o cluster ira usar para determinar a prioridade da requisicao
         timestamp = datetime.datetime.now().isoformat()
         
         solicitacao_json = {
@@ -45,6 +53,7 @@ def iniciar_cliente():
             "timestamp": timestamp
         }
 
+        # abre uma conexao com o peer, o cliente nao espera para sempre, se o algo falhar ou nenhuma resposta chegar em 30 segundos o cliente desiste e um erro eh lancado
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(30)
@@ -52,12 +61,16 @@ def iniciar_cliente():
                 s.sendall(json.dumps(solicitacao_json).encode('utf-8'))
                 print(f"[{CLIENT_ID}]: Enviou solicitação {i+1} com timestamp {timestamp}.")
 
+                # espera pela resposta do servidor, que pode ser COMMITTED ou um erro
                 data = s.recv(1024)
                 if not data:
                     print(f"[{CLIENT_ID}]: Conexão fechada pelo servidor.")
                     continue
                     
                 data = data.decode('utf-8')
+
+                # verifica se a resposta eh COMMITED, caso de sucesso
+                # usa o recurso e o loop continua para a proxima requisicao
                 if data == "COMMITTED":
                     print(f"[{CLIENT_ID}]: Recebeu COMMITTED para solicitação {i+1}. Acesso concedido.")
                     acessos_realizados += 1
@@ -84,7 +97,5 @@ def iniciar_cliente():
     print(f"[{CLIENT_ID}]: Finalizado. Total de {acessos_realizados} acessos concedidos ao Recurso R.")
 
 
-
 if __name__ == "__main__":
     iniciar_cliente()
-
--- cluster_sync_app/cluster_sync_peer.py
+++ cluster_sync_app/cluster_sync_peer.py
@@ -2,13 +2,13 @@ import socket
 import threading
 import time
 import random
-import datetime
+#import datetime
 import json
 import os
 import sys
-import traceback
+#import traceback
 
-# --- Configurações Iniciais ---
+# configuracoes iniciais do peer, serao lidos das variaveis de ambiente do docker compose
 CLUSTER_ID = os.getenv("PEER_ID", f"Peer_{random.randint(1000, 9999)}")
 HOST = '0.0.0.0'
 
@@ -21,22 +21,28 @@ except (ValueError, TypeError) as e:
     print(f"[{CLUSTER_ID}] ERRO FATAL: Falha na leitura da PEER_PORT. Detalhe: {e}", file=sys.stderr)
     sys.exit(1)
 
+# lista de peers do cluster para que cada peer saiba quem sao os outros
 CLUSTER_MEMBERS = [
     ("Peer_1", "sync_peer_1", 12345), ("Peer_2", "sync_peer_2", 12346),
     ("Peer_3", "sync_peer_3", 12347), ("Peer_4", "sync_peer_4", 12348),
     ("Peer_5", "sync_peer_5", 12349)
 ]
 
-# --- Estado Global do Peer ---
-lock = threading.Lock()
-pending_requests = []
-my_current_request = None
-ok_responses_received = 0
-client_sockets_for_reply = {}
-deferred_oks = {}
-peer_ready = threading.Event()
+# estado global do peer, variaveis compartilhadas entre threads
+lock = threading.Lock() # garante que apenas uma thread acesse as variaveis compartilhadas por vez
+pending_requests = [] # fila de espera global, armazena tupla de todas as requisicoes pendentes no sistema
+my_current_request = None # requisicao atual do peer, se ele nao estiver na fila, sera None
+ok_responses_received = 0 # contador de respostas OK recebidas para a requisicao atual
+client_sockets_for_reply = {} # dicionario que mapeia client_id para o socket do cliente que deve receber a resposta
+deferred_oks = {} # dicionario que armazena OKs que devem ser enviados mais tarde, requisicao do proprio peer eh mais prioritaria
+peer_ready = threading.Event() # semaforo liberado quando todos os peers estao prontos, impedindo processamento de requisicoes antes do sistema estar estavel
+
 
 def send_message(host, port, message, retries=5, initial_timeout=1.0):
+    '''
+    envia uma mensagem para um peer especificado por host e porta.
+    retorna True se a mensagem foi enviada com sucesso, ou False se houve falha.
+    '''
     for attempt in range(retries):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -54,11 +60,19 @@ def send_message(host, port, message, retries=5, initial_timeout=1.0):
     return False
 
 def broadcast_to_peers(message):
+    '''
+    envia uma mensagem para todos os peers do cluster, exceto o próprio peer.
+    '''
     for peer_id, peer_host, peer_port in CLUSTER_MEMBERS:
         if peer_id != CLUSTER_ID:
             send_message(peer_host, peer_port, message)
 
+
 def broadcast_request_to_peers(client_id, client_timestamp):
+    '''
+    define a requisicao atual, adiciona na propria fila de espera.
+    envia uma mensagem de requisicao REQUEST_CLUSTER para todos os outros peers.
+    '''
     global my_current_request, ok_responses_received
     with lock:
         my_current_request = (client_timestamp, client_id, CLUSTER_ID)
@@ -72,12 +86,26 @@ def broadcast_request_to_peers(client_id, client_timestamp):
     broadcast_to_peers(request_msg)
     process_ok_response(CLUSTER_ID, CLUSTER_ID)
 
+
 def handle_client_request(conn, addr, message):
+    '''
+    funcao chamada quando cliente envia requisicao.
+    guarda o socket do cliente para enviar resposta depois
+    chama broadcast_request_to_peers para iniciar processo de votacao
+    '''
     with lock:
         client_sockets_for_reply[message["client_id"]] = conn
     broadcast_request_to_peers(message["client_id"], message["timestamp"])
 
+
 def handle_cluster_request(message):
+    '''
+    funcao executada quando outro peer envia uma requisicao REQUEST_CLUSTER
+    adiciona a requisicao a sua fila local pending_requests e reordena a fila
+    compara o timestamp da requisicao recebida com o timestamp da requisicao atual do peer
+    se ele nao tem requisicao atual ou se o pedido recebido eh mais antigo, envia OK para o peer que fez a requisicao
+    se nao, adia a resposta e adiciona o peer solicitante ao dicionario deferred_oks
+    '''
     global deferred_oks
     req_peer_id = message["requester_peer_id"]
     new_request_tuple = (message["client_timestamp"], message["client_id"], req_peer_id)
@@ -95,7 +123,13 @@ def handle_cluster_request(message):
         else:
             deferred_oks[req_peer_id] = True
 
+
 def process_ok_response(responding_peer_id, target_peer_id):
+    '''
+    funcao chamada no peer original toda vez que recebe uma resposta OK de outro peer
+    incrementa o ok_responses_received e quando chega ao numero de peers do cluster faz a verificacao final
+    se a requisicao atual eh a primeira da fila, chama execute_critical_section
+    '''
     global ok_responses_received
     if target_peer_id != CLUSTER_ID: return
     with lock:
@@ -105,10 +139,13 @@ def process_ok_response(responding_peer_id, target_peer_id):
         if ok_responses_received == len(CLUSTER_MEMBERS) and pending_requests[0] == my_current_request:
             execute_critical_section()
 
-# ============================================================================
-# ADIÇÃO: Handler para a nova mensagem RELEASE
-# ============================================================================
+
 def handle_release_message(message):
+    '''
+    funcao executada em todos os peers quando recebem a mensagem RELEASE de outro peer que liberou o recurso
+    simplesmente remove a requisicao finalizada da sua fila de pending_requests
+    apos remover o item, ela checa se por acaso agora eh a sua vez de entrar na secao critica (caso ja tenha os 5 ok)
+    '''
     global pending_requests
     with lock:
         request_to_remove = (message["client_timestamp"], message["client_id"], message["releasing_peer_id"])
@@ -121,28 +158,32 @@ def handle_release_message(message):
 
 
 def execute_critical_section():
+    '''
+    funcao chamada quando um peer ganha o direito de acesso ao recurso compartilhado
+    '''
     global pending_requests, my_current_request, ok_responses_received, deferred_oks
 
 
     print(f"[{CLUSTER_ID}] *** SEÇÃO CRÍTICA INICIADA para {my_current_request[1]} ***")
     
-    # Prepara dados para I/O antes de modificar o estado
+    # prepara dados para I/O antes de modificar o estado
     req_tuple = my_current_request
     client_socket = client_sockets_for_reply.pop(req_tuple[1], None)
     
-    # Modifica o estado atômicamente
+    # modifica o estado atomicamente com lock
+    # remove sua requisicao da fila de espera, limpa variaveis de estado e pega lista de oks adiados para enviar depois
     pending_requests.pop(0)
     my_current_request = None
     ok_responses_received = 0
     peers_to_send_ok = list(deferred_oks.keys())
     deferred_oks.clear()
     
-    # O lock será liberado aqui. Operações lentas ocorrem abaixo.
-    
+    # simula o trabalho na secao critica
     sleep_time = random.uniform(0.2, 1)
     print(f"[{CLUSTER_ID}] ...Trabalhando por {sleep_time:.2f}s...")
     time.sleep(sleep_time)
     
+    # envia COMMITTED para o cliente que fez a requisicao
     if client_socket:
         try:
             client_socket.sendall(b"COMMITTED")
@@ -152,14 +193,12 @@ def execute_critical_section():
 
     print(f"[{CLUSTER_ID}] *** SEÇÃO CRÍTICA LIBERADA ***")
 
-    # ============================================================================
-    # ALTERAÇÃO: Transmitir a mensagem RELEASE para todos os outros peers
-    # ============================================================================
+    # transmite uma mensagem RELEASE para todos os peers, sinalizando que terminou e o recurso esta livre para uso
     release_msg = { "type": "RELEASE", "releasing_peer_id": CLUSTER_ID,
                     "client_id": req_tuple[1], "client_timestamp": req_tuple[0] }
     broadcast_to_peers(release_msg)
 
-    # Enviar OKs que estavam adiados
+    # envia as mensagens OK que estavam na sua lista deferred_oks
     for peer_id in peers_to_send_ok:
         target_host, target_port = next(((p[1], p[2]) for p in CLUSTER_MEMBERS if p[0] == peer_id), (None, None))
         if target_host:
@@ -168,6 +207,11 @@ def execute_critical_section():
 
 
 def handle_connection(conn, addr):
+    '''
+    lida com conexoes de clientes ou outros peers.
+    le a mensagem recebuida, decodifica e olha o campo type no JSON.
+    Com base no tipo, chama a funcao apropriada para processar a mensagem.
+    '''
     try:
         data = conn.recv(1024).decode('utf-8')
         if not data: conn.close(); return
@@ -188,6 +232,9 @@ def handle_connection(conn, addr):
 
 
 def check_cluster_readiness():
+    '''
+    verifica se todos os peers do cluster estao prontos para processar requisicoes.
+    '''
     print(f"[{CLUSTER_ID}] Verificando prontidão do cluster...")
     ready_peers = {CLUSTER_ID}
     while len(ready_peers) < len(CLUSTER_MEMBERS):
@@ -202,6 +249,11 @@ def check_cluster_readiness():
 
 
 def start_peer_server():
+    '''
+    loop infinito que aceita conexoes de rede, para cada nova conexao ele cria uma thread 
+    para executar handle_connection.
+    Assim, o servidor atende multiplas conexoes simultaneamente.
+    '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
@@ -212,8 +264,11 @@ def start_peer_server():
             threading.Thread(target=handle_connection, args=(conn, addr), daemon=True).start()
 
 
+
 if __name__ == "__main__":
     print(f"[{CLUSTER_ID}] Peer iniciando... Meu ID: {CLUSTER_ID}, Endereço: {HOST}:{PORT}")
+    # duas threads: uma inicia o servidor TCP qe fica escutando para sempre por conexoes de clientes ou outros peers
+    # outra executa a verificacao que garante que todos os nós estao de pe, permitindo que o sistema comece a processar requisições
     server_thread = threading.Thread(target=start_peer_server, daemon=True)
     readiness_thread = threading.Thread(target=check_cluster_readiness, daemon=True)
     server_thread.start()
